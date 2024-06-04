9ca4dabfcc36a3b96a7ef009e8ca9243b9534e32
StefiManci <stefi.manci1@gmail.com>
2024-06-04 07:54:50 -0700

    Removed unnecessary spaces

--- Startup.cs
+++ Startup.cs
@@ -1,17 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using MusicStore.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MusicStore
 {
@@ -28,7 +21,7 @@ namespace MusicStore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddMvc(options => options.EnableEndpointRouting=false);
+            services.AddMvc(options => options.EnableEndpointRouting = false);
             services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(Configuration["Data:MusicStoreMusics:ConnectionString"]));
             services.AddTransient<IAlbumRepository, EFAlbumRepository>();
@@ -37,22 +30,19 @@ namespace MusicStore
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddSession();
             services.AddMemoryCache();
-            
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-          
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSession();
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseMvc(
-               routes=>
+               routes =>
                {
                    routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}");
                    routes.MapRoute(name: "", template: "{controller}/{action}");
@@ -65,6 +55,5 @@ namespace MusicStore
                 );
             SeedData.EnsurePopulated(app);
         }
-       
     }
 }
